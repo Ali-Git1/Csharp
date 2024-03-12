ed63ed17ddc74949f1739e52eead0afa4479bcf3
Ali Aliyev <elvinaliyev724@gmail.com>
2024-03-12 11:03:31 +0400

    task finished

---
+++ ConsoleApp3A/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task1 
+
+            //int[] nums = { 5, 4, 2, 11, 7, 6,9,13 };
+            //int cut = 0;
+            //int tek = 0;
+            //for (int i = 0; i < nums.Length; i++)
+            //{
+            //    //Console.WriteLine(nums[i]);
+            //    if (nums[i] %2==0)
+            //    {
+            //        cut++;
+            //    }
+            //    else
+            //    {
+            //        tek++;
+
+            //    }
+            //}
+            //Console.WriteLine(tek);
+            //}
+
+
+            //Task 2
+            //int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
+            //int max = arr[0];
+            //int min = arr[0];
+            //int cem = 0;
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    if (max < arr[i])
+            //    {
+            //        max = arr[i];
+            //    }
+            //    if (min > arr[i])
+            //    {
+            //        min = arr[i];
+            //    }
+            //}
+            //Console.WriteLine("Massivin en boyuk elementi:" + max);
+            //Console.WriteLine("Massivin en kicik elementi:" + min);
+            //cem = (max + min);
+            //Console.WriteLine("Massivin en kicik ve en boyuk elemantlerinin cemi:" + cem);
+
+
+
+            //Task 3
+            //    int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
+            //    int max = arr[0];
+            //    int min = arr[0];
+            //    int ferq = 0;
+            //    for (int i = 0; i < arr.Length; i++)
+            //    {
+            //        if (max < arr[i])
+            //        {
+            //            max = arr[i];
+            //        }
+            //        if(min > arr[i])
+            //        {
+            //            min = arr[i];
+            //        }
+            //    }
+            //    Console.WriteLine("Massivin en boyuk elementi:"+max);
+            //    Console.WriteLine("Massivin en kicik elementi:"+min);
+            //    ferq = (max - min);
+            //    Console.WriteLine("Massivin en kicik ve en boyuk elemantlerinin ferqi:"+ferq);
+            //}
+
+
+
+            //Task4
+            //int[] nums = { 5, 1, 4, 7, 2, 3, 6 };
+            //int sirala = 0;
+            //for (int i = 0; i < nums.Length-1; i++)
+            //{
+            //    for (int j = i+1; j < nums.Length; j++)
+            //    {
+            //        if (nums[i] > nums[j])
+            //        {
+            //            sirala = nums[i];
+            //            nums[i] = nums[j];
+            //            nums[j] = sirala;
+            //        }
+            //    }
+            //}
+            //Console.WriteLine("Artan sira ile ");
+            //foreach (var item in nums)
+            //{
+            //    Console.Write(item +" ");
+            //}
+
+
+
+
+            int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, };
+            int tek = 0;
+            int cut = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                //Console.WriteLine(nums[i]);
+                if (nums[i]%2== 0)
+                {
+                    
+                    cut += nums[i];
+                    
+                   
+
+                }
+                else
+                {
+                  
+                    tek += nums[i];
+                   
+                }
+            }
+            Console.WriteLine("Cut ededlerin cemi :"+cut);
+            
+            Console.WriteLine("Tek ededlerin cemi :"+tek);
+            
+
+        }
+
+    }
+    
+}
